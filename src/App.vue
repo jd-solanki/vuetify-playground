763804d497e9147b039ee2df72d77715ff451e3a
jd-solanki <jdsolanki0001@gmail.com>
2023-03-29 12:17:23 +0530

    updated font size to 15px

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <v-app>
+  <v-app style="background: #eee">
     <v-main>
-      <HelloWorld />
+      <div class="ma-8">
+        <VCard>
+          <v-card-text>
+            <v-text-field density="compact" variant="outlined" label="E For Checking Vertical Alignment"></v-text-field>
+          </v-card-text>
+        </VCard>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
 </script>
