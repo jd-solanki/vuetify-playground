01d6c4960c3fc60a16512d74528c72b1bff7e11a
JD <jdsolanki0001@gmail.com>
2023-03-30 07:03:49 +0000

    added alert

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
         <VCard>
           <v-card-text>
             <v-text-field density="compact" variant="outlined" label="E For Checking Vertical Alignment"></v-text-field>
+            <v-alert>E For Checking</v-alert>
           </v-card-text>
         </VCard>
       </div>
